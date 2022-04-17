4a5738ef02bd5340ca0a4639cf198f966d275f2d
Mohit <mohitpratapsingh198@gmail.com>
2022-04-18 00:22:17 +0530

    Test Done

---
+++ Test-4/index.js
@@ -0,0 +1,52 @@
+const container = document.getElementById("container")
+console.log();
+const userList = [{
+    "id": 1,
+    "email": "george.bluth@reqres.in",
+    "first_name": "George",
+    "last_name": "Bluth",
+    "avatar": "https://reqres.in/img/faces/1-image.jpg"
+}, {
+    "id": 2,
+    "email": "janet.weaver@reqres.in",
+    "first_name": "Janet",
+    "last_name": "Weaver",
+    "avatar": "https://reqres.in/img/faces/2-image.jpg"
+}, {
+    "id": 3,
+    "email": "emma.wong@reqres.in",
+    "first_name": "Emma",
+    "last_name": "Wong",
+    "avatar": "https://reqres.in/img/faces/3-image.jpg"
+}, {
+    "id": 4,
+    "email": "eve.holt@reqres.in",
+    "first_name": "Eve",
+    "last_name": "Holt",
+    "avatar": "https://reqres.in/img/faces/4-image.jpg"
+}, {
+    "id": 5,
+    "email": "charles.morris@reqres.in",
+    "first_name": "Charles",
+    "last_name": "Morris",
+    "avatar": "https://reqres.in/img/faces/5-image.jpg"
+}, {
+    "id": 6,
+    "email": "tracey.ramos@reqres.in",
+    "first_name": "Tracey",
+    "last_name": "Ramos",
+    "avatar": "https://reqres.in/img/faces/6-image.jpg"
+}]
+
+userList.forEach(e => {
+    container.innerHTML += `<div class="detail">
+<div class="upper">
+    <img src=${e.avatar} alt="">
+    <span id="ids">${e.id}</span>
+</div>
+<div class="lower">
+    <h4 id="firstname">${e.first_name} ${e.last_name}</h4>
+    <h4 id="lastname">${e.email}</h4>
+</div>
+</div>`
+})
